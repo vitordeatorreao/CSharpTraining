8838760212f8b8374a773fbb85bd28ab9db4471e
Vitor Torreao <torreao@pm.me>
2022-07-03 13:19:08 -0300

    Convert controller into async

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -8,16 +8,16 @@ namespace BankApi.Controllers;
 public class AccountsController : ControllerBase
 {
     private readonly ILogger<AccountsController> _logger;
-    private readonly IAccountDAL _accountDAL;
+    private readonly IAccountDALAsync _accountDAL;
 
-    public AccountsController(ILogger<AccountsController> logger, IAccountDAL accountDAL)
+    public AccountsController(ILogger<AccountsController> logger, IAccountDALAsync accountDAL)
     {
         _logger = logger;
         _accountDAL = accountDAL;
     }
 
     [HttpPost]
-    public ActionResult<BankAccountResponse> Create(BankAccountRequest request)
+    public async Task<ActionResult<BankAccountResponse>> Create(BankAccountRequest request)
     {
         var validationError = ValidateRequest(request);
         if (validationError != null) return validationError;
@@ -30,6 +30,7 @@ public class AccountsController : ControllerBase
             CanGoNegative = request.CanGoNegative.HasValue ? request.CanGoNegative.Value : false,
             // No need to set Balance, as its defaul value is what we want.
         };
+        var createdAccount = await _accountDAL.Create(account);
         return CreatedAtAction(
             // nameOf returns the name of a class, method, field or property as a string.
             // Using your IDE's rename feature will affect this string as well, so this can be
@@ -37,33 +38,35 @@ public class AccountsController : ControllerBase
             // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/nameof
             nameof(Read),
             new { id = account.Id },
-            ToResponse(_accountDAL.Create(account)));
+            ToResponse(account));
     }
 
     [HttpGet("{id}")]
-    public ActionResult<BankAccountResponse> Read(Guid id)
+    public async Task<ActionResult<BankAccountResponse>> Read(Guid id)
     {
-        var account = _accountDAL.ReadById(id);
+        var account = await _accountDAL.ReadById(id);
         if (account == null) return NotFound();
         return ToResponse(account);
     }
 
     [HttpPut("{id}")]
-    public ActionResult<BankAccountResponse> Update(Guid id, BankAccountRequest request)
+    public async Task<ActionResult<BankAccountResponse>> Update(Guid id, BankAccountRequest request)
     {
-        var account = _accountDAL.ReadById(id);
+        var account = await _accountDAL.ReadById(id);
         if (account == null) return NotFound();
         if (!string.IsNullOrWhiteSpace(request.Name)) account.Name = request.Name;
         if (!string.IsNullOrWhiteSpace(request.OwnerName)) account.OwnerName = request.OwnerName;
-        return ToResponse(_accountDAL.Update(account));
+        var updatedAccount = await _accountDAL.Update(account);
+        return ToResponse(updatedAccount);
     }
 
     [HttpDelete("{id}")]
-    public ActionResult<BankAccountResponse> Delete(Guid id)
+    public async Task<ActionResult<BankAccountResponse>> Delete(Guid id)
     {
         try
         {
-            return ToResponse(_accountDAL.DeleteById(id));
+            var deletedAccount = await _accountDAL.DeleteById(id);
+            return ToResponse(deletedAccount);
         }
         catch (AccountNotFoundException)
         {
@@ -72,9 +75,9 @@ public class AccountsController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<BankAccountResponse> List()
+    public async IAsyncEnumerable<BankAccountResponse> List()
     {
-        foreach (var account in _accountDAL.List())
+        foreach (var account in await _accountDAL.List())
         {
             yield return ToResponse(account);
         }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<IAccountDAL, AccountDAO>(opt =>
+builder.Services.AddDbContext<IAccountDALAsync, AccountDAOAsync>(opt =>
     opt.UseInMemoryDatabase("AccountList"));
 
 var app = builder.Build();
